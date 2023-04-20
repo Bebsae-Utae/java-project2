359a0650b0b1ea7005944af23066bce12d4843fe
nzxt <tm1361@naver.com>
2023-04-20 11:37:26 +0900

    feat: 코드업1009~1010, 구구단 - Codeup1009~1010, GugudanEx 생성

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1009.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.codeup;
+import com.example.javaproject2.week1.ScannerEx;
+
+import java.util.Scanner;
+public class Codeup1009 {
+    public void InputData(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt());
+    }
+    public static void main(String[] args) {
+        Codeup1009 id = new Codeup1009();
+        id.InputData();
+
+    }
+}
