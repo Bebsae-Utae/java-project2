4c04090e3a96eeb68bad461ba9b52c27dccea0b9
nzxt <tm1361@naver.com>
2023-04-20 09:22:24 +0900

    feat: 코드업1001~1005 + 스캐너 + 버퍼리더 - Codeup1001~1005, ScannerEx, BufferedReaderEx 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1004.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.codeup;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("'Hello'");
+    }
+}
--- src/main/java/com/example/javaproject2/week1/BufferedReaderEx.java
+++ src/main/java/com/example/javaproject2/week1/BufferedReaderEx.java
@@ -12,4 +12,10 @@ public class BufferedReaderEx {
         System.out.println(br.readLine());
 
     }
+
+    public void plusTwoNumbers() throws IOException {
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);
+        System.out.println(br.readLine() + br.readLine());
+    }
 }
