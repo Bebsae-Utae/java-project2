7389abb924ada4dd61dac495e1ade32575078235
O11XL <tm1361@naver.com>
2023-04-21 14:41:33 +0900

    feat: public 및 oop 실습(method 돌려쓰기) - Point, PointTest 수정

--- src/main/java/com/example/javaproject2/week1/day5/Point.java
+++ src/main/java/com/example/javaproject2/week1/day5/Point.java
@@ -1,8 +1,12 @@
 package com.example.javaproject2.week1.day5;
 
 public class Point {
-    private int x;
-    private int y;
+    public int x;
+    public int y;
+
+    boolean isSameXy() {
+        return x==y;
+    }
 
     public static void main(String[] args) {
         Point p = new Point();
