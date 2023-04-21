707e06cfbffa8b0eb75c6952ffedb81bf1c02615
O11XL <tm1361@naver.com>
2023-04-21 14:31:03 +0900

    feat: private 실습 - Point 생성

---
+++ src/main/java/com/example/javaproject2/week1/day5/Point.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week1.day5;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public static void main(String[] args) {
+        Point p = new Point();
+        System.out.println(p.x);
+    }
+}
