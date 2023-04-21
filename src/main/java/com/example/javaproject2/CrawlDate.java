b67c41f21c2af1769f124137623f3297de8f92ed
O11XL <tm1361@naver.com>
2023-04-21 10:47:45 +0900

    feat: 새로만든 git ignore 제거 - 잘못만듦

--- src/main/java/com/example/javaproject2/CrawlDate.java
+++ src/main/java/com/example/javaproject2/CrawlDate.java
@@ -1,10 +1,10 @@
-package com.example.javaproject2;
-
-public class CrawlDate {
-    private LocalDateTime lastCrawlDateTime;
-    // alt + insert 누르면 컨스트럭터 생성 가능
-
-    public CrawlDate(LocalDateTime lastCrawlDateTime) {
-        this.lastCrawlDateTime = lastCrawlDateTime;
-    }
-}
+//package com.example.javaproject2;
+//
+//public class CrawlDate {
+//    private LocalDateTime lastCrawlDateTime;
+//    // alt + insert 누르면 컨스트럭터 생성 가능
+//
+//    public CrawlDate(LocalDateTime lastCrawlDateTime) {
+//        this.lastCrawlDateTime = lastCrawlDateTime;
+//    }
+//}
