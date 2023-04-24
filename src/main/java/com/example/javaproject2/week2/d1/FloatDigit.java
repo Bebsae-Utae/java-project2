1bfb64a6aa17757e83ef4f95fabd41bab7e656c1
O11XL <tm1361@naver.com>
2023-04-24 11:29:02 +0900

    feat: 타입 실습3 - float과 double 예제

---
+++ src/main/java/com/example/javaproject2/week2/d1/FloatDigit.java
@@ -0,0 +1,22 @@
+package com.example.javaproject2.week2.d1;
+
+public class FloatDigit {
+    public static void main(String[] args) {
+        float f7 = 1.23e7f;
+        float f8 = 1.23e8f;
+        float f9 = 1.23e9f;
+        float f10 = 1.23e10f;
+        double f11 = 1.23e10;
+
+        System.out.println("f7 = " + f7);
+        System.out.println("f8 = " + f8);
+        System.out.println("f9 = " + f9);
+        System.out.println("f10 = " + f10);
+        System.out.println("f11 = " + f11);
+
+        float f1 = 3.141592653589793f;
+        System.out.println(f1);
+        double f2 = 3.141592653589793f;
+        System.out.println(f2);
+    }
+}
