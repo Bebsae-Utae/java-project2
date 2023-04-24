de8657c0ec706754800e9e29a4321e429daa9d87
O11XL <tm1361@naver.com>
2023-04-24 10:04:25 +0900

    feat: 타입 실습 - DefineAVariable, VariableEx 생성

---
+++ src/main/java/com/example/javaproject2/week2/d1/DefineAVariable.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.week2.d1;
+
+public class DefineAVariable {
+    public static void main(String[] args) {
+        int num = 10;
+        String name = "김떡만";
+        String name2 = new String("김고만");
+
+        String sOne = new String("1");
+        int iOne = 1;
+        float fOne = 1.0f;
+
+
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week2/d1/VariableEx.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week2.d1;
+
+public class VariableEx {
+    public static void main(String[] args) {
+        String sOne = new String("1");
+        int iOne = 1;
+        float fOne = 1.0f;
+
+        System.out.printf("StringL%s Number:%d Float:%f", sOne, iOne, fOne);
+    }
+}
